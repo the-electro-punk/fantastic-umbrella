733fd23882966187a5d32997b74f68cf1db5fec5
the-electro-punk <bwr2555@hotmail.com>
2021-11-11 15:23:01 -0600

    updated code to include connection

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+
+// this connects the JS code to the database
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'Gizmonic',
+  port: '3306',
+})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,3 +22,5 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+// be able to connect to a database, download seed data, then input data???
